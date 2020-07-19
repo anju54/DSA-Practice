c7405408b5e90d949c07411e1287570ead12516a
anju54 <anju.k302@gmail.com>
2020-07-19 19:58:53 +0530

    Added recursive approach for fibonacci

---
+++ src/Recursion/Fibonacci.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class Fibonacci {
+
+    public static int fib(int n){
+        if(n==0)
+            return 0;
+        if(n==1 || n==2)
+            return 1;
+
+        return fib(n-1) + fib(n-2) ;
+    }
+
+    public static void main(String[] args) {
+        int num = 9;
+        int res = fib(num);
+        System.out.println(res);
+    }
+}
