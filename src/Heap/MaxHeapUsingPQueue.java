0c6cdf70701efdbf0c1059bc65a9916d97e0a5da
anju54 <anju.k302@gmail.com>
2020-11-15 18:11:06 +0530

    Max heap using priority queue

---
+++ src/Heap/MaxHeapUsingPQueue.java
@@ -0,0 +1,25 @@
+package Heap;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class MaxHeapUsingPQueue {
+
+    public static void main(String[] args) {
+
+        // Creating empty priority queue
+        PriorityQueue<Integer> pQueue =
+                new PriorityQueue<Integer>(Collections.reverseOrder());
+
+        pQueue.add(10);
+        pQueue.add(30);
+        pQueue.add(20);
+        pQueue.add(400);
+
+        System.out.println("Head value using peek function:" +
+                pQueue.peek());
+
+    }
+
+
+}
