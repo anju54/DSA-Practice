cb8e21b2d4e7954de9f6fc33bed03bf7d5d6de86
anju54 <anju.k302@gmail.com>
2021-05-09 01:09:55 +0530

    fixes

--- src/Stack/NGL.java
+++ src/Stack/NGL.java
@@ -3,7 +3,7 @@ package Stack;
 import java.util.Arrays;
 import java.util.Stack;
 
-// NGL | Nearest Greater to left
+// NGL Next Greater Element |
 public class NGL {
 
     public static Integer[] calNgl(Integer[] arr){
@@ -12,29 +12,49 @@ public class NGL {
 
         Integer ans[] = new Integer[arr.length];
 
-        for(int i=0;i<arr.length;i++){
-            if(stack.empty())
+        for (int i=arr.length-1;i>=0;i--){
+            if (stack.isEmpty())
                 ans[i] = -1;
-            else if(stack.peek()>arr[i])
-                ans[i]= stack.peek();
-            else if(stack.peek()<=arr[i]){
-                // pop the top till stack is not empty and  top is lesser than the current element
-                while( stack.peek()<=arr[i] && !stack.empty()){
+            else if (stack.peek() > arr[i]) {
+                ans[i] = stack.peek();
+                //System.out.println(ans[i]);
+            }
+            else{
+                while ( stack.size()>0 && stack.peek() <= arr[i] ){
                     stack.pop();
                 }
-                // so now loop has terminated so now check if stack is empty thats why loop has terminated
-                if(stack.empty())
-                    ans[i]=-1;
-                else // otherwise the top item is bigger than the current so loop has terminated
+                if (stack.size() == 0)
+                    ans[i] = -1;
+                else
                     ans[i] = stack.peek();
             }
-        stack.push(arr[i]);
+
+            stack.push(arr[i]);
         }
+
+//        for(int i=0;i<arr.length;i++){
+//            if(stack.empty())
+//                ans[i] = -1;
+//            else if(stack.peek()>arr[i])
+//                ans[i]= stack.peek();
+//            else if(stack.peek()<=arr[i]){
+//                // pop the top till stack is not empty and  top is lesser than the current element
+//                while( stack.peek()<=arr[i] && !stack.empty()){
+//                    stack.pop();
+//                }
+//                // so now loop has terminated so now check if stack is empty thats why loop has terminated
+//                if(stack.empty())
+//                    ans[i]=-1;
+//                else // otherwise the top item is bigger than the current so loop has terminated
+//                    ans[i] = stack.peek();
+//            }
+//        stack.push(arr[i]);
+//        }
     return ans;
     }
 
     public static void main(String[] args) {
-        Integer [] arr = {40, 20, 30, 10};
+        Integer arr[]= {1, 3, 2, 4};
 
         System.out.println(Arrays.asList(calNgl(arr)));
     }
