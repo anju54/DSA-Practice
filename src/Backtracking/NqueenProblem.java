3efdd2413b136ccbcd50d8af7a92bb280dd5b07a
anju54 <anju.k302@gmail.com>
2021-05-20 16:36:37 +0530

    Custom hash of emp and hashcode and equals method implemented

--- src/Backtracking/NqueenProblem.java
+++ src/Backtracking/NqueenProblem.java
@@ -1,79 +1,75 @@
 package Backtracking;
 
+// https://www.geeksforgeeks.org/n-queen-problem-backtracking-3/
 public class NqueenProblem {
 
-    public static boolean isAttack(int row, int col, int board[][], int N) {
-        int k, l;
-        // checking for column j
-        for(k=1; k<=row-1; k++) {
-            if(board[k][col] == 1)
-                return true;
+    // return true if that place is safe
+    public static boolean isAttack(int row, int col, int[][] board, int N) {
+
+        int i,j;
+
+        // check left
+        for ( i = 0; i < col; i++) {
+            if (board[row][i] == 1)
+                return false;
         }
 
-        // checking upper right diagonal
-        k = row-1;
-        l = col+1;
-        while (k>=1 && l<=N) {
-            if (board[k][l] == 1)
-                return true;
-            k=k+1;
-            l=l+1;
+        // check upper diagonal on left side
+        for (i=row, j=col; i >= 0 && j >= 0 ;i--,j--){
+            if (board[i][j] == 1)
+                return false;
+        }
+        // check lower diagonal on left side
+        for (i=row, j=col; i < N && j >= 0 ; i++, j--  ){
+            if (board[i][j] == 1)
+                return false;
         }
 
-        // checking upper left diagonal
-        k = row-1;
-        l = col-1;
-        while (k>=1 && l>=1) {
-            if (board[k][l] == 1)
-                return true;
-            k=k-1;
-            l=l-1;
-        }
-
-        return false;
+        return true;
     }
 
-    /**
-     * @param row
-     * @param n number of queen
-     * @param N length of the board
-     * @param board
-     * @return
-     */
-    public static boolean nQueen(int row, int n, int N, int board[][]) {
-        if (n==0)
+    public static boolean nQueen(int N, int[][] board,int col) {
+
+        if (col >= N)
             return true;
 
-        for (int j=1; j<=N; j++) {
-            if(!isAttack(row, j, board, N)) {
-                board[row][j] = 1;
-
-                // Check if possible to place next queen
-                if (nQueen(row+1, n-1, N, board))
+        for (int i=0; i<N; i++){
+            if ( isAttack(i,col,board,N)){
+                board[i][col] = 1;
+                if (nQueen(N, board, col + 1))
                     return true;
-
-                // If we reached here that means it is not possible to place the queen so now backtrack
-                board[row][j] = 0; //backtracking
+                //backtrack
+                board[i][col] = 0;
             }
         }
         return false;
     }
 
     public static void main(String[] args) {
-        int[][] board = new int[5][5];
+        
+        int[][] board = new int[4][4];
 
-        for(int row=0;row<=4;row++) {
-            for(int col=0;col<=4;col++)
+        for(int row=0;row<4;row++) {
+            for(int col=0;col<4;col++)
                 board[row][col] = 0;
         }
 
-        nQueen(1, 4, 4, board);
-
-        //printing the matix
-        for(int i=1;i<=4;i++) {
-            for(int j=1;j<=4;j++)
+        //printing the matrix
+        System.out.println("Input ------");
+        for(int i=0;i<4;i++) {
+            for(int j=0;j<4;j++)
                 System.out.print(board[i][j]+"\t");
-            System.out.println("");
+            System.out.println();
+        }
+
+        nQueen(4, board,0);
+
+        System.out.println("output ----");
+        //printing the matrix
+        for(int i=0;i<4;i++) {
+            for(int j=0;j<4;j++)
+                System.out.print(board[i][j]+"\t");
+            System.out.println();
         }
     }
 }
