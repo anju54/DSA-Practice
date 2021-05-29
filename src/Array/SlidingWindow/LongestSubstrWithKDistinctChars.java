02fa56ad0935e903b43a9813d89519beaa33ebaf
anju54 <anju.k302@gmail.com>
2021-05-29 22:59:53 +0530

    leetcode 340. Longest Substring with At Most K Distinct Characters

--- src/Array/SlidingWindow/LongestSubstrWithKDistinctChars.java
+++ src/Array/SlidingWindow/LongestSubstrWithKDistinctChars.java
@@ -2,6 +2,13 @@ package Array.SlidingWindow;
 
 import java.util.HashMap;
 
+/**
+ * Given a string s and an integer k, return the length of the longest substring of s
+ * that contains at most k distinct characters.
+ * Input: s = "eceba", k = 2
+ * Output: 3
+ * Explanation: The substring is "ece" with length 3.
+ */
 // from grokking the coding interview
 public class LongestSubstrWithKDistinctChars {
 
