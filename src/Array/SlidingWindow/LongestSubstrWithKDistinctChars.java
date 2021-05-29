ef91de2e4e9be5df855b6b74ff254f6ee39e4c9e
anju54 <anju.k302@gmail.com>
2021-05-29 22:58:21 +0530

    leetcode 340. Longest Substring with At Most K Distinct Characters

---
+++ src/Array/SlidingWindow/LongestSubstrWithKDistinctChars.java
@@ -0,0 +1,39 @@
+package Array.SlidingWindow;
+
+import java.util.HashMap;
+
+// from grokking the coding interview
+public class LongestSubstrWithKDistinctChars {
+
+    public static void main(String[] args) {
+        String s = "eceba" ; int k = 2;
+        int res = solve(s,k);
+        System.out.println(res);
+    }
+
+    public static int solve(String s,int k){ // s = "araaci" k = 2
+
+        HashMap<Character,Integer> hashMap = new HashMap<Character, Integer>();
+        int maxLen = 0;
+        int start = 0;
+
+        for(int end=0;end<s.length();end++){
+
+            Character ch = s.charAt(end);
+            hashMap.put(ch, hashMap.getOrDefault(ch,0)+1);
+
+            // if map size is more than k
+            while (hashMap.size()>k){
+
+                hashMap.put(ch, hashMap.get(ch) - 1);
+                if(hashMap.get(ch) == 0){
+                    hashMap.remove(ch);
+                }
+                start++;
+            }
+        maxLen = Math.max(maxLen, (end - start) + 1); // end -start + 1 bcz end will point to index value
+        }
+        return maxLen;
+    }
+
+}
